1a1558e4ef262bee3f8dca21e1e91ac58bec4d61
k0nghaa <k0nghaalee@gmail.com>
2026-02-17 15:33:23 +0900

    feat(dropdown): item 선택/액션 처리 및 클릭 시 close 추가
    - disabled UX 조건부

---
+++ src/components/compound/Dropdown/DropdownItem.tsx
@@ -0,0 +1,40 @@
+import type { ReactNode } from "react";
+import { useDropdownContext } from "../../../provider/DropdownProvider";
+
+type DropdownItemProps = {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  value: string;
+  onSelect?: (value: string) => void;
+};
+
+export default function DropdownItem({
+  children,
+  className,
+  disabled,
+  value,
+  onSelect,
+}: DropdownItemProps) {
+  const { close } = useDropdownContext();
+
+  const onClick = () => {
+    if (disabled) return;
+    // Select 에 대한 로직은 사용처에서
+    onSelect?.(value);
+    close();
+  };
+
+  return (
+    <li
+      onClick={onClick}
+      className={
+        disabled
+          ? `cursor-not-allowed opacity-50 ${className}`
+          : `cursor-pointer hover:bg-gray-100 ${className}`
+      }
+    >
+      {children}
+    </li>
+  );
+}
